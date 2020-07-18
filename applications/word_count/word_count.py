533803ebaad0061f95b0b2d60ae01eaa0d9971db
ash12hub <ash12hub@users.noreply.github.com>
2020-07-18 10:17:20 +0530

    word count

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,6 +1,28 @@
 def word_count(s):
     # Your code here
+    split_characters = ['"', ':', ';', ',', '.', '-',
+    '+', '=', '/', '\\', '\r', '\t', '\n', '|', '[', ']', '{', '}', '(',
+    ')', '*', '^', '&', ' ']
+    words = {}
+    word_start = 0
 
+    for i in range(len(s)):
+        if s[i] in split_characters:
+            word = s[word_start:i].lower()
+            word_start = i + 1
+            if len(word) > 0 and word not in split_characters:
+                if word not in words:
+                    words[word] = 1
+                else:
+                    words[word] += 1
+        elif i == len(s) - 1:
+            word = s[word_start:].lower()
+            if len(word) > 0 and word not in split_characters:
+                if word not in words:
+                    words[word] = 1
+                else:
+                    words[word] += 1
+    return words
 
 
 if __name__ == "__main__":
